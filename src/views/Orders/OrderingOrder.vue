704587f8ec536886acf35e5233cf6807f588decb
Volodymyr Troian <volodymyr.troian@intenselab.com>
2024-08-04 19:09:53 +0300

    deleted imports

--- src/views/Orders/OrderingOrder.vue
+++ src/views/Orders/OrderingOrder.vue
@@ -183,7 +183,9 @@
 </template>
 
 <script setup lang="ts">
+import { ErrorMessage, Field, Form as VForm } from "vee-validate";
 import { object, string, boolean } from "yup";
+
 
 const basket = useBasketStore();
 const order = useOrderStore();
